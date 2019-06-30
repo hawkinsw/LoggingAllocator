0199f0bfdb05b8391f8aac5fa8d2b79966b20d7a
Will Hawkins <whh8b@obs.cr>
2019-06-29 20:42:56 -0400

    Add a vector example (and move things around)

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
-all: test
+CC=g++
+#CC=clang++
 
-test: main.cpp alloc.h
-	g++ -o test main.cpp -g -O0 -std=c++17
+all: string_test vector_test
+
+string_test: string_main.cpp alloc.h
+	$(CC) -o string_test string_main.cpp -g -O0 -std=c++17
+
+vector_test: vector_main.cpp alloc.h
+	$(CC) -o vector_test vector_main.cpp -g -O0 -std=c++17
 
 alloc.h:
 	true
+
+clean:
+	rm -f string_test vector_test *.o core
---
+++ vector_main.cpp
@@ -0,0 +1,98 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "allocate.h"
+constexpr std::string_view allocate_tag = "allocate";
+constexpr std::string_view deallocate_tag = "deallocate";
+
+using allocd_vector = std::vector<int, LoggingAllocator<int, std::allocator<int>, allocate_tag, deallocate_tag> >;
+
+void make_very(allocd_vector &vec) {
+	std::cerr << "(start) make_very" << std::endl;
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	std::cerr << "(stop) make_very" << std::endl;
+}
+
+void make_really(allocd_vector &vec) {
+	std::cerr << "(start) make_really" << std::endl;
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	vec.push_back(5);
+	make_very(vec);
+	std::cerr << "(stop) make_really" << std::endl;
+}
+
+int main(void) {
+	allocd_vector vec = {0};
+	make_really(vec);
+	return 0;
+}
